30b1d87a13b172b40b370cad5a045d736d1a7454
Kevin Bluer <kevin@bluer.com>
2020-08-04 17:07:37 -0500

    updating the payees

--- migrations/2_deploy_paymentsplitter.js
+++ migrations/2_deploy_paymentsplitter.js
@@ -1,5 +1,5 @@
 const PaymentSplitter = artifacts.require("PaymentSplitter");
 
 module.exports = function(deployer) {
-  deployer.deploy(PaymentSplitter, ['0x94AF10Cb6fdC971cf1861206B40B55d5716FBBDe', '0x00D5d0Fb287ed5D492F185604333aD80ce0E58Df'], [123, 321]);
+  deployer.deploy(PaymentSplitter, ['0x54688E3d1C2A9F03c7c73f85f80C6b48F7546b8F', '0xF144cB01033F58400E290970eF4975852509B3c5'], [40, 60]);
 };
